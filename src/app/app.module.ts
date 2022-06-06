fc32e7ccea9338e5bdb046b6b664d5d2172ac621
Gabriele-Romoli <gabriele.romoli@comm-it.it>
2022-06-06 10:28:54 +0200

    da fixare

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LearnTableComponent } from './learn-table/learn-table.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ButtonConfirmComponent } from './button-confirm/button-confirm.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LearnTableComponent,
+    ButtonConfirmComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/button-confirm/button-confirm.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+/**
+ * @title Basic buttons
+ */
+@Component({
+  selector: 'button-overview-example',
+  templateUrl: 'button-confirm.component.html',
+  styleUrls: ['button-confirm.component.css'],
+})
+export class ButtonConfirmComponent {}
